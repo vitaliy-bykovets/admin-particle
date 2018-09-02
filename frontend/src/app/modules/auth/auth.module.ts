8127c39cf17046a99b64fe93d80e05b2933e1b19
vadim <ivadimdobrydnyk@gmail.com>
2018-09-02 20:03:59 +0300

    add style, add auth forms validation

--- frontend/src/app/modules/auth/auth.module.ts
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AuthComponent } from './containers/auth/auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import {AuthRouterModule} from "./auth-router.module";
+import { AuthRouterModule } from "./auth-router.module";
+
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthRouterModule
+    AuthRouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [AuthComponent, LoginComponent, RegisterComponent]
 })
